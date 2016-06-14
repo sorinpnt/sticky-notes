62ec40b04e5037bc40bb48cd76542a0a43b959e1
Pintea Sorin <pintea.sorin2007@gmail.com>
2016-06-14 14:29:32 +0300

    Downgraded angular ui select due to bugs, and set default priority and status on new notes

--- js/components/add-note/add-note-ctrl.js
+++ js/components/add-note/add-note-ctrl.js
@@ -1,15 +1,14 @@
 var addNote = function( $scope, $uibModal, NOTE_PRIORITY, NOTE_STATUS ) {
-	var _ = this;
 	this.selected = { value: ''};
-	$scope.$watch(_.selected.value, function(newVal, oldVal) {
-		console.info(_.selected)
-	});
+	
 	this.modalTitle = 'Add Note';
 	this.priorities = NOTE_PRIORITY;
 	this.status = NOTE_STATUS;
 	this.note = {
 		title: '',
-		body: ''
+		body: '',
+		priorityId: this.priorities[0].id,
+		statusId: this.status[0].id
 	};
 
 };
